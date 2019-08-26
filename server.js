6194784c231bb2c27b4427b2a51456e1be689038
Nikola Radovic <nikola.radovic@devtech.local>
2019-08-26 13:06:56 +0200

    make it better1

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const app = express();
 
 //Connect Database
 // connectDB()
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 //Init Middleware to get data from req.body in models
 app.use(express.json({ extended: false }));
 
@@ -31,5 +31,5 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 //Server runs on port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
