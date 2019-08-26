d58c9b929cd77706cc6b10fa0f89d0caa5b46903
Nikola Radovic <nikola.radovic@devtech.local>
2019-08-26 13:31:03 +0200

    make it better7

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const app = express();
 
 //Connect Database
 // connectDB()
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 //Init Middleware to get data from req.body in models
 app.use(express.json({ extended: false }));
 
