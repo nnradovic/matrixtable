7f2cf39d5509eb926cdb3a3b56e7e045808b6926
Nikola Radovic <nikola.radovic@devtech.local>
2019-08-26 12:54:12 +0200

    production setup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require('path')
 //Server UP
 const app = express();
 
@@ -20,6 +20,15 @@ app.use(express.json({ extended: false }));
 
 app.use("/products", require("./routes/products"));
 app.use("/settings", require("./routes/settings"));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+   app.use(express.static('client/build'))
+   app.get('*', (req,res)=>{
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+   })
+}
 
 //Server runs on port
 const PORT = process.env.PORT || 5000;
